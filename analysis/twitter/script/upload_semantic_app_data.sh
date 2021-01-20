e4fe72cd926faee155943aae8d17d38d65ab1197
ldscholes <39913137+ldscholes@users.noreply.github.com>
2021-01-20 14:06:36 +0000

    twitter semantic app scripts

---
+++ analysis/twitter/script/upload_semantic_app_data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# upload_semantic_app_data.sh
+#
+###############################################################################################################################
+
+# Call Standard Profile
+BASEDIR="~/redJester/analysis/twitter"
+HQL_DIR=${BASEDIR}/hql
+SCRIPT_DIR=${BASEDIR}/script
+
+
+echo "BASEDIR="$BASEDIR
+echo "HQL_DIR="$HQL_DIR
+echo "SCRIPT="$SCRIPT_DIR
+
+
+HQL_NAME="upload_semantic_app_table.hql"
+# upload semantic app table
+echo "--INFO-- Script ${HQL_DIR}/${HQL_NAME} Running..."
+cd ${HQL_DIR}
+hive -f ~/redJester/analysis/twitter/hql/upload_semantic_app_table.hql
+
+ if [ $? -ne 0 ]
+        then
+                echo "--ERROR-- Script ${HQL_DIR}/${HQL_NAME} FAILED"
+                exit 1
+        else
+                echo "--INFO-- Script ${HQL_DIR}/${HQL_NAME} Completed Successfully"
+ fi
+
